b33b377bcc285189d0e2c1418a73462785104f6f
fayannie <zw19870201@hotmail.com>
2012-03-07 19:57:57 +0100

    add validation

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,8 @@
 class Picture < ActiveRecord::Base
   # Paperclip
-  has_attached_file :photo, :styles => { :small => "150*150>"}
+  has_attached_file :photo, :styles => { :small => ["150*150>", :jpg] }
+
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png']
 end
